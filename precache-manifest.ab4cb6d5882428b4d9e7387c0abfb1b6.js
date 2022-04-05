0129ab2acf944e72ff68c8f89e4d06081d4afb44
anderson383 <anderson383@users.noreply.github.com>
2022-04-05 22:32:24 +0000

    deploy: 1d89ffbe8da0c564d826b8682f9a9bc1ee51455d

---
+++ precache-manifest.ab4cb6d5882428b4d9e7387c0abfb1b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8896897f0c7736a3de0d1abce5fdfa73",
+    "url": "https://anderson383.github.io/task-mangement/index.html"
+  },
+  {
+    "revision": "12f5cd274245ec57db22",
+    "url": "https://anderson383.github.io/task-mangement/static/css/main.bf3abc0b.chunk.css"
+  },
+  {
+    "revision": "dd4823bd746096198cf3",
+    "url": "https://anderson383.github.io/task-mangement/static/js/2.b7cc748c.chunk.js"
+  },
+  {
+    "revision": "12f5cd274245ec57db22",
+    "url": "https://anderson383.github.io/task-mangement/static/js/main.52a8c593.chunk.js"
+  },
+  {
+    "revision": "f37aa20851e89c292c0c",
+    "url": "https://anderson383.github.io/task-mangement/static/js/runtime~main.e50d4e8a.js"
+  }
+]);
